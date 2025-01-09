5ce073ebbdb959d902f3a010463c87634f1c9ccf
tajciglar <taj.brzovic@gmail.com>
2025-01-09 17:00:45 +0100

    Changes to the backend structure

---
+++ backend/prisma/createAdmin.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+async function main(): Promise<void> {
+    try {
+        const hashedPass = await bcrypt.hash("admin123", 10);
+
+        const admin = await prisma.user.create({
+            data: {
+                username: "Admin",
+                email: "admin@example.com",
+                password: hashedPass,
+                bio: "This is admin's profile",
+            },
+        });
+
+        console.log("Admin created:", admin);
+    } catch (error) {
+        console.error("Error creating admin:", error);
+    } finally {
+        await prisma.$disconnect();
+    }
+}
+
+main().catch((error) => {
+    console.error("Unexpected error:", error);
+    process.exit(1);
+});
